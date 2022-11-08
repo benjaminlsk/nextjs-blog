56144ec33c428c76d9f346f8fc105ea4459374be
Benjamin LISZEWSKI <benjamin@heybilly.io>
2022-11-08 13:46:32 +0100

    Base-setup

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Read <a href="/posts/first-posts">this page !</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
